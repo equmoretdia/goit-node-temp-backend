94bfcf07b7d2de32f586301e2a7767a78c3483dc
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-17 19:00:34 +0100

    books collection json and file with methods of collection are removed, and new mongoose schema and model are added since we'll use remote database

--- controllers/books.js
+++ controllers/books.js
@@ -1,57 +1,60 @@
 const books = require("../models/books");
 
-const { HttpError, ctrlWrapper } = require("../helpers");
+const {
+  // HttpError,
+  ctrlWrapper,
+} = require("../helpers");
 
 const getAll = async (req, res) => {
   const allBooks = await books.getAll();
   res.json(allBooks);
 };
 
-const getById = async (req, res) => {
-  // console.log(req.params);
-  const { id } = req.params;
-  const oneBook = await books.getById(id);
-  if (!oneBook) {
-    throw HttpError(404, "Not found");
-    //   const error = new Error("Not found");
-    //   error.status = 404;
-    //   throw error;
-    //   ***
-    //   return res.status(404).json({ message: "Not found" });
-  }
-  res.json(oneBook);
-};
+// const getById = async (req, res) => {
+//   // console.log(req.params);
+//   const { id } = req.params;
+//   const oneBook = await books.getById(id);
+//   if (!oneBook) {
+//     throw HttpError(404, "Not found");
+//     //   const error = new Error("Not found");
+//     //   error.status = 404;
+//     //   throw error;
+//     //   ***
+//     //   return res.status(404).json({ message: "Not found" });
+//   }
+//   res.json(oneBook);
+// };
 
-const add = async (req, res) => {
-  const newBook = await books.add(req.body);
-  res.status(201).json(newBook);
-};
+// const add = async (req, res) => {
+//   const newBook = await books.add(req.body);
+//   res.status(201).json(newBook);
+// };
 
-const updateById = async (req, res) => {
-  const { id } = req.params;
-  const result = await books.updateById(id, req.body);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.json(result);
-};
+// const updateById = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await books.updateById(id, req.body);
+//   if (!result) {
+//     throw HttpError(404, "Not found");
+//   }
+//   res.json(result);
+// };
 
-const deleteId = async (req, res) => {
-  const { id } = req.params;
-  const result = await books.deleteById(id);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  // res.status(204).send()
-  res.json({
-    message: "Delete success",
-  });
-};
+// const deleteId = async (req, res) => {
+//   const { id } = req.params;
+//   const result = await books.deleteById(id);
+//   if (!result) {
+//     throw HttpError(404, "Not found");
+//   }
+//   // res.status(204).send()
+//   res.json({
+//     message: "Delete success",
+//   });
+// };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  getById: ctrlWrapper(getById),
-  add: ctrlWrapper(add),
-  updateById: ctrlWrapper(updateById),
-  deleteId: ctrlWrapper(deleteId),
+  // getById: ctrlWrapper(getById),
+  // add: ctrlWrapper(add),
+  // updateById: ctrlWrapper(updateById),
+  // deleteId: ctrlWrapper(deleteId),
 };
