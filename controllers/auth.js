1a1c9e5473e98d8cab028646cdfb1bc3491e4132
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-20 23:00:39 +0100

    register controller is updated with password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
+const bcrypt = require("bcrypt");
+
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const registeredEmail = await User.findOne({ email });
   if (registeredEmail) {
     throw HttpError(
@@ -11,7 +13,8 @@ const register = async (req, res) => {
       "User with submitted email has been registered already"
     );
   }
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({ name: newUser.name, email: newUser.email });
 };
 
