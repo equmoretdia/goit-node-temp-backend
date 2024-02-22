abe83b6d58ba39f4dc3ade20b33ffa3c623b3e74
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-22 17:30:54 +0100

    authenticate middleware is added, all routes are updated

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,25 +4,32 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/books");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/book");
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
 
-router.delete("/:id", isValidId, ctrl.deleteId);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteId);
 
 module.exports = router;
