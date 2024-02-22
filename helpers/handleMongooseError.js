c64e1dcdadfe6a7b27bb61e08db3b70516ada81f
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-22 17:55:42 +0100

    helpers update

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,10 +1,10 @@
 const handleMongooseError = (error, data, next) => {
   // console.log(error);
   // console.log(error.name);
-  error.status = 400;
-  // const { name, code } = error;
-  // const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
-  // error.status = status;
+  // error.status = 400;
+  const { name, code } = error;
+  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  error.status = status;
   // error.message = ""
   next();
 };
